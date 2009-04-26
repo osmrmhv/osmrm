a9c8813ef2d5f9f7787b7175be90fd5346797d61
Candid Dauth <cdauth@cdauth.de>
2009-04-26 21:22:39 +0000

    Added license note.

--- classes/gui.php
+++ classes/gui.php
@@ -38,6 +38,25 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<!--
+	This file is part of OSM Route Manager.
+
+	OSM Route Manager is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	OSM Route Manager is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with OSM Route Manager.  If not, see <http://www.gnu.org/licenses/>.
+
+	Obtain the source code from http://svn.cdauth.de/viewvc.cgi/Tools/osm/route-manager/
+	or svn://svn.cdauth.de/tools/osm/route-manager/.
+-->
 	<head>
 		<title><?=htmlspecialchars($title)?></title>
 		<link rel="stylesheet" href="style.css" type="text/css" />
